7bfbd84137f33148cb21b080233b5b85ebc3b4c1
Ewright2021 <78454700+Ewright2021@users.noreply.github.com>
2021-07-05 15:14:36 -0400

    added remaining pages and transitions

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 gsap.fromTo("#home-title", {opacity: 0, y: -200, ease: "elastic"}, {opacity: 1, y:0, duration: .7,  } );
-gsap.to("#home-byline", {opacity: 1, delay: 1});
-gsap.fromTo("#home-message", {scale: 0, ease: "elastic"},{scale: 1, delay: 1.2, duration: .3})
+gsap.to("#home-byline", {opacity: 1, delay: .6});
+gsap.fromTo("#home-message", {scale: 0, ease: "elastic"},{scale: 1, delay: .8, duration: .3});
+gsap.fromTo("#instructions", {scale: 0,}, {scale: 1, delay: 1, duration: .3});
+
+// Page Transitions
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 1, background: "#ba181b"
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
+});
