c3dd3e407123d822869b51ed5fe6046a7e8d7c9d
Ewright2021 <78454700+Ewright2021@users.noreply.github.com>
2021-07-05 19:23:45 -0400

    terminated #view-work statement

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ gsap.fromTo("#home-title", {opacity: 0, y: -200, ease: "elastic"}, {opacity: 1, 
 gsap.to("#home-byline", {opacity: 1, delay: .6});
 gsap.fromTo("#home-message", {scale: 0, ease: "elastic"},{scale: 1, delay: .8, duration: .3});
 gsap.fromTo("#instructions", {scale: 0,}, {scale: 1, delay: 1, duration: .3});
-gsap.fromTo("#view-work", {opacity: 0, y: 300}, {opacity: 1, y: 0, delay: 1.2, duration: .3})
+gsap.fromTo("#view-work", {opacity: 0, y: 300}, {opacity: 1, y: 0, delay: 1.2, duration: .3});
 
 // Page Transitions
 // barba.init({
