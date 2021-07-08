fd6ac91587fda1d6aecf460aa716e90f7f6bf91f
Ewright2021 <78454700+Ewright2021@users.noreply.github.com>
2021-07-07 21:13:45 -0400

    latest commit

---
+++ wordpress/front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); 
+/*
+Template Name: Single Column Template
+Template Post Type: post, page
+*/ ?>
+<main data-barba="container" data-barba-namespace="home"> 
+  <div class="container">
+    <h1 class="text-center mx-auto" id="home-title">Innovative Software UI Designer </h1>
+    <h2 class="text-center mx-auto mb-3" id="home-byline">.....Who Can Also<span class="text-primary" id="byline-accent"> Code</span>!</h2>
+    <div class="row"> 
+      <div class="col-12 col-lg-6 mx-auto text-center">
+        <p class="lead text-center mx-auto" id="home-message">Hi! I'm E. Wright, a graduate of the Art Institute of Atlanta, with over 15 years of professional experience developing dynamic user interfaces for websites, software and mobile applications.</p><a class="btn btn-outline-primary" id="view-work" href="./work.html">View Work </a>
+      </div>
+    </div>
+  </div>
+</main>
+<?php get_footer(); ?>
